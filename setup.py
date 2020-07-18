81806dee20fc31cc1d6581a9f41b8e8707caa4e6
davidho27941 <jimsam3456@cycu.org.tw>
2020-07-19 00:23:51 +0800

    BREAKING CHANGE: Add setup.py, can be installed by Defaulting to user installation because normal site-packages is not writeable Processing /home/david/workplace/HEAH Requirement already satisfied: numpy>=1.19.0 in /home/david/.local/lib/python3.6/site-packages (from HEAH==0.1.0) (1.19.0) Requirement already satisfied: pandas==1.0.5 in /home/david/.local/lib/python3.6/site-packages (from HEAH==0.1.0) (1.0.5) Requirement already satisfied: pytz>=2017.2 in /usr/lib/python3/dist-packages (from pandas==1.0.5->HEAH==0.1.0) (2018.3) Requirement already satisfied: python-dateutil>=2.6.1 in /usr/lib/python3/dist-packages (from pandas==1.0.5->HEAH==0.1.0) (2.6.1) Building wheels for collected packages: HEAH   Building wheel for HEAH (setup.py): started   Building wheel for HEAH (setup.py): finished with status 'done'   Created wheel for HEAH: filename=HEAH-0.1.0-py3-none-any.whl size=2176 sha256=78cd434f207f8decf238e27563b20f643508ceeb1409ad9aed05d306bc624a19   Stored in directory: /tmp/pip-ephem-wheel-cache-d8asaa4g/wheels/8d/7e/31/843e8c83d04f066cf7e68f0e7946e3f83e8e8b723991a728d9 Successfully built HEAH Installing collected packages: HEAH Successfully installed HEAH-0.1.0 in the future.

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='HEAH', 
+        version='0.1.0',
+        author='Ta-Wei Ho',
+        author_email='davidho@gapp.nthu.edu.tw',
+        maintainer='Ta-Wei Ho',
+        maintainer_email='davidho@gapp.nthu.edu.tw',
+        licence='OSI Approved, MIT License',
+        keywords='High energy physics',
+        python_requires='>=3.6',
+        setup_requires=[
+            'setuptools==39.0.1',
+            'pytest-runner>=3.0',
+            ],
+        install_requires=[
+            'pandas==1.0.5',
+            'numpy>=1.19.0',
+            ],
+        tests_require=[
+            'pytest>=3.3.1',
+            'pytest-cov>=2.5.1',
+            ],
+        classifiers = [
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers :: HEP students',
+            'Topic :: Software Development :: Build Tools',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3.6.9',
+            ],
+        url='https://github.com/davidho27941/HEAH',
+        zip_safe=False)
